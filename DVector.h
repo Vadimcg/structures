c629e4aa79dd517b7b104160cbb13959ec9cd34b
vadimcg <vadim-derevich@yandex.ru>
2016-10-27 08:27:01 +0300

    I made template realization in vector

--- DVector.h
+++ DVector.h
@@ -8,35 +8,117 @@
 #define STRUCTURES_DVECTOR_H
 
 
-class DVector {
+template<class T> class DVector {
+
 private:
     const static int MIN_CAPACITY=12;
 
-    std::unique_ptr<int[]> data_;
+    std::unique_ptr<T[]> data_;
     int size_{0};
     int capacity_{MIN_CAPACITY};
 
-    void ensureCapacity(int capacity);
-    void increaseSize(int capacity);
-    void decreaseSize(int capacity);
+    void ensureCapacity(int capacity){
+        if(capacity_<=capacity)
+            this->increaseSize(capacity);
+        else
+            this->decreaseSize(capacity);
+    }
 
-    void setNewCapacity();
+    void increaseSize(int capacity){
+
+        int new_capacity=this->capacity_;
+        do {
+            new_capacity =new_capacity*3/2+1;
+        }while(new_capacity<capacity);
+
+        capacity_=new_capacity;
+
+        this->setNewCapacity();
+    }
+
+    void decreaseSize(int capacity){
+        int new_capacity=capacity_;
+        if(capacity<capacity_/3){
+
+            do{
+                new_capacity=new_capacity/3*2+1;
+            }while(new_capacity>capacity*3);
+
+            if(new_capacity<MIN_CAPACITY)
+                new_capacity=MIN_CAPACITY;
+
+            capacity_=new_capacity;
+            this->setNewCapacity();
+        }
+    }
+
+    void setNewCapacity(){
+        std::unique_ptr<T[]> new_data(new T[capacity_]);
+
+        for(int i=0;i<size_;i++){
+            new_data[i]=data_[i];
+        }
+
+        data_=std::move(new_data);
+    }
 
 
 
 public:
-    DVector();
-    DVector(const DVector& vector);
-    ~DVector();
 
-    int getSize() const;
-    bool  isEmpty() const;
-    int getCapacity()const;
+    DVector(){
+        this->data_=std::unique_ptr<T[]>(new T[MIN_CAPACITY]);
+    }
 
-    bool push(int value);
-    int getValueAt(int index) const;
-    bool deleteAt(int index);
+    DVector(const DVector& vector){
 
+    }
+
+    ~DVector(){
+        this->data_.reset();
+    }
+
+    int getSize() const{
+        return this->size_;
+    }
+
+    bool  isEmpty() const{
+        return size_==0;
+    }
+
+    int getCapacity()const{
+        return this->capacity_;
+    }
+
+
+    bool push(T value){
+        this->ensureCapacity(this->size_+1);
+        this->data_[size_++]=value;
+    }
+
+    T getValueAt(int index) const{
+        if(index>=size_ || index<0){
+            std::cout << "Index out of bounds." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        return this->data_[index];
+    }
+
+    bool deleteAt(int index){
+        if(index>=size_ || index<0){
+            std::cout << "Index out of bounds." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        this->decreaseSize(size_-1);
+
+        for(int i=index;i<size_-1;i++){
+            this->data_[i]=this->data_[i+1];
+        }
+
+        this->size_--;
+    }
 
 
 };
--- main.cpp
+++ main.cpp
@@ -4,21 +4,23 @@
 int main() {
     std::cout << "Test vector:" << std::endl;
 
-    DVector vector;
+    DVector<std::string> vector;
 
+    vector.push("a1");
+    vector.push("a2");
+    vector.push("a3");
+    vector.push("a4");
+    vector.push("a5");
+    vector.push("a6");
+    vector.push("a7");
+    vector.push("a8");
+    vector.push("a9");
 
-    for(int i=0;i<100;i++)
-        vector.push(i);
-
-
-    for(int i=99;i>15;i--)
-        vector.deleteAt(i);
 
 
     std::cout << "Capacity:"<<vector.getCapacity()<< std::endl;
     for(int i=0;i<vector.getSize();i++){
         std::cout << vector.getValueAt(i) << std::endl;
     }
-
     return 0;
 }
