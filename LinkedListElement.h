7f78ec8bd172a686532c14e1bb718e331923a85f
vadimcg <vadim-derevich@yandex.ru>
2016-10-31 22:14:30 +0300

    Started making LinkedList

---
+++ LinkedListElement.h
@@ -0,0 +1,40 @@
+//
+// Created by vadimcg on 31.10.16.
+//
+
+#import <memory>
+
+#ifndef STRUCTURES_LINKEDLISTELEMENT_H
+#define STRUCTURES_LINKEDLISTELEMENT_H
+
+template<class T>
+class LinkedListElement {
+
+private:
+    LinkedListElement* next_;
+    T data_;
+
+public:
+    LinkedListElement(const T& data):next_(nullptr),data_(data){}
+    ~LinkedListElement(){}
+
+    const T& getData() const {
+        return this->data_;
+    }
+
+    LinkedListElement<T> getLinkedListElement() const{
+        return this;
+    }
+
+    void setData(const T& data){
+        this->data_=data;
+    }
+
+    void setNextElement(LinkedListElement* next){
+        this->next_=next;
+    }
+
+};
+
+
+#endif //STRUCTURES_LINKEDLISTELEMENT_H
