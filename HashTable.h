c7423d61e035c479356f79002f68ad60ed97a387
vadimcg <vadim-derevich@yandex.ru>
2016-11-12 23:13:10 +0300

    Started making hash function

--- HashTable.h
+++ HashTable.h
@@ -3,6 +3,7 @@
 //
 
 #import <iostream>
+#include <typeinfo.h>
 #import "HashTableObject.h"
 
 #ifndef STRUCTURES_HASHTABLE_H
@@ -12,19 +13,56 @@ template<class T,class F>
 class HashTable {
 
 private:
-        HashTableObject* objects;
-        int size_;
-public:
-        explicit HashTable(int size){
 
-            if(size<=0){
-                std::cout << "Incorrect size!" << std::endl;
-                exit(EXIT_FAILURE);
-            }
+    HashTableObject* objects;
+    int size_;
 
-            this->size_=size;
-            this->objects=new HashTableObject[size];
+    unsigned int makeHash(T key){
+
+        unsigned int hash = 0;
+
+        for (; *key; key++)
+        {
+            hash += (unsigned char)(*key);
+            hash += (hash << 10);
+            hash ^= (hash >> 6);
         }
+        hash += (hash << 3);
+        hash ^= (hash >> 11);
+        hash += (hash << 15);
+
+        return hash;
+    }
+
+public:
+    explicit HashTable(int size){
+
+        if(size<=0){
+            std::cout << "Incorrect size!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        this->size_=size;
+        this->objects=new HashTableObject[size];
+    }
+
+
+    ~HashTable(){
+        delete[] objects;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 };
 
