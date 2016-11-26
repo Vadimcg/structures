928118c2ff1cfc0e8ddc9461ba41b0207a033ae0
vadimcg <vadim-derevich@yandex.ru>
2016-11-26 22:28:25 +0300

    Created dequeue and enqueue functions

--- Queue.h
+++ Queue.h
@@ -2,6 +2,8 @@
 // Created by vadimcg on 24.11.16.
 //
 
+#include <iostream>
+
 #ifndef STRUCTURES_QUEUE_H
 #define STRUCTURES_QUEUE_H
 
@@ -28,6 +30,20 @@ private:
         this->amount_=0;
     }
 
+
+    void resize(){
+        int newSize=this->size_*3/2+1;
+
+        T*newQueuePointer=new T[newSize];
+
+        for(int i=0;i<this->size_;i++){
+            newQueuePointer[i]=this->queuePointer_[i];
+        }
+
+        this->queuePointer_=newQueuePointer;
+        this->size_=newSize;
+    }
+
 public:
 
     Queue():size_{MIN_SIZE}{
@@ -48,7 +64,33 @@ public:
 
     void enqueue(T& element){
 
+        if(amount_+1==size_)
+            this->resize();
+
+        this->queuePointer_[++amount_]=element;
     }
+
+
+    T& dequeue(){
+
+        if (amount_ == 0) {
+            std::cerr << "Queue is empty!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        T& outElement= this->queuePointer_[0];
+
+        for(int i=1;i<amount_;i++){
+            this->queuePointer_[i-1]=this->queuePointer_[i];
+        }
+
+        amount_--;
+
+        return outElement;
+    }
+
+
+
 
 
 
